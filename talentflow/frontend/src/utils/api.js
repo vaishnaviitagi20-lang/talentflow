2987ccdcd87ded3717f91460887b1cb10ed8935e
Vaishnavi Itagi <vaiishnavii206@gmail.com>
2026-04-19 10:25:00 +0530

    initial commit

--- talentflow/frontend/src/utils/api.js
+++ talentflow/frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://talentflow-p2jy.onrender.com/api",
+  baseURL: "https://talentflow-p2jy.onrender.com/",
   headers: { "Content-Type": "application/json" },
   timeout: 15000,
 });
