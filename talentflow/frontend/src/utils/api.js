2c7a8372ec9652bf91b4151aad608cc09ee72e2d
Vaishnavi Itagi <vaiishnavii206@gmail.com>
2026-04-20 17:20:53 +0530

    fixed baseURL

--- talentflow/frontend/src/utils/api.js
+++ talentflow/frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://talentflow-p2jy.onrender.com/",
+  baseURL: "https://talentflow-p2jy.onrender.com",
   headers: { "Content-Type": "application/json" },
   timeout: 15000,
 });
